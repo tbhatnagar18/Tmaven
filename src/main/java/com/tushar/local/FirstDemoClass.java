61cd6dc7036e3f91cba395b281d99cdb1b6aa33e
Tushar Bhatnagar <tbhatnagar@bravurasolutions.com>
2020-08-29 13:06:25 +0530

    my first maven commit

---
+++ src/main/java/com/tushar/local/FirstDemoClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.tushar.local;
+
+/**
+ * @author tbhatnagar
+ *
+ */
+public class FirstDemoClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/tushar/local/FirstDemoClassTest.java
@@ -0,0 +1,14 @@
+package com.tushar.local;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirstDemoClassTest {
+
+	@Test
+	public void test() {
+		/*fail("Not yet implemented");*/
+	}
+
+}
